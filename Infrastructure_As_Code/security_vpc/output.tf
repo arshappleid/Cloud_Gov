d82fa7736195c0c99308f38ba27c35b26248f381
arshappleid <arsh.appleid@gmail.com>
2024-01-17 22:16:03 -0800

    Configured the Transit gateway connect

--- Infrastructure_As_Code/security_vpc/output.tf
+++ Infrastructure_As_Code/security_vpc/output.tf
@@ -2,3 +2,8 @@ output "instance_ids" {
   value       = aws_vpc.security_vpc.cidr_block
   description = "Security VPC CIDR"
 }
+
+output "transit_gateway_id"{
+	value = aws_ec2_transit_gateway.Main_Transity_Gateway.id
+	description = "Transit Gateway Id"
+}
--- Infrastructure_As_Code/security_vpc/transit_gateway.tf
+++ Infrastructure_As_Code/security_vpc/transit_gateway.tf
@@ -2,12 +2,17 @@ resource "aws_ec2_transit_gateway" "Main_Transity_Gateway" {
   description = "Main Transit Gateway"
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "example_tgw_attachment" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
   transit_gateway_id = aws_ec2_transit_gateway.Main_Transity_Gateway.id
   vpc_id             = aws_vpc.security_vpc.id
   subnet_ids         = [aws_subnet.security_vpc_public_subnet.id]
 }
 
-resource "aws_ec2_transit_gateway_route_table" "example_tgw_rt" {
-  transit_gateway_id = aws_ec2_transit_gateway.example_tgw.id
+resource "aws_ec2_transit_gateway_route_table" "tgw_route_table" {
+  transit_gateway_id = aws_ec2_transit_gateway.Main_Transity_Gateway.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "example" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
 }
