a4618194bcdaaa448716e2dce77563081bcde22c
arshappleid <arsh.appleid@gmail.com>
2024-01-24 23:52:13 -0800

    Updated READMe

--- iam_roles/organization_accounts.tf
+++ iam_roles/organization_accounts.tf
@@ -1,10 +1,11 @@
 resource "aws_organizations_organization" "cloud_gov" {
   feature_set = "ALL"
 }
-
-
-
-
+resource "aws_organizations_policy_attachment" "tier1_scp_attachment_1" {
+  policy_id = aws_organizations_policy.tier1_upper_management_scp.id
+  target_id = aws_organizations_organization.cloud_gov.id
+}
+// Rest of the accounts
 
 resource "aws_organizations_account" "marketing_account" {
   name  = "Marketing Department"
