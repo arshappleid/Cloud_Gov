7a98b8dbf8192525b7978a69183a2e8a8d12c980
arshappleid <arsh.appleid@gmail.com>
2024-03-01 23:43:30 -0800

    Completed Provisioning SSM Keys

--- Infrastructure_As_Code/ssm_parameters/kms_key.tf
+++ Infrastructure_As_Code/ssm_parameters/kms_key.tf
@@ -1,6 +1,18 @@
 resource "aws_kms_key" "database_info_key" {
-  description = "KMS key for database info"
-  customer_master_key_spec = "RSA_2048"
-  key_usage = "ENCRYPT_DECRYPT"
-  deletion_window_in_days = 30 // if accidentally deleted, till how long to configure it
+  description              = "KMS key for database info"
+  customer_master_key_spec = "RSA_2048" // Assymetric key
+  key_usage                = "ENCRYPT_DECRYPT"
+  deletion_window_in_days  = 30 // if accidentally deleted, till how long to configure is
+}
+// Create Administration Policy
+resource "aws_kms_key_policy" "database_key_admin_policy" {
+  key_id = aws_kms_key.database_info_key.id
+  policy = file("${path.module}/policies/key_admin_policy.json")
+}
+// Attach the Above policy to anyone , who you want to give admin acces for the key
+
+// Create Key Usage Policy - For EC2 Instances
+resource "aws_kms_key_policy" "database_key_usage_policy" {
+  key_id = aws_kms_key.database_info_key.id
+  policy = file("${path.module}/policies/key_usage_policy.json")
 }
--- Infrastructure_As_Code/ssm_parameters/parameters.tf
+++ Infrastructure_As_Code/ssm_parameters/parameters.tf
@@ -1,24 +1,30 @@
 resource "aws_ssm_parameter" "database_password" {
-  name = "/production/database/password/master"
+  name        = "/production/database/password/master"
   description = "Password for master database, for production environment"
-  type = "SecureString"
-  value = "thisIsTheDatabasePassword"
+  type        = "SecureString"
+  value       = "thisIsTheDatabasePassword"
+  key_id      = aws_kms_key.database_info_key.id
   tags = {
-	Environment = "production"
+    Environment = "production"
   }
 }
 
 resource "aws_ssm_parameter" "database_username" {
-  name = "/production/database/username/master"
+  name        = "/production/database/username/master"
   description = "Username for master database, for production environment"
-  type = "String"
-  value = "thisIsTheDatabaseUsername"
+  type        = "String"
+  value       = "thisIsTheDatabaseUsername"
+  key_id      = aws_kms_key.database_info_key.id
   tags = {
-	Environment = "production"
+    Environment = "production"
   }
 }
 resource "aws_ssm_parameter" "database_url" {
-	name = "/production/database/url"
-	description = "URL for master database, for production environment"
-	type = "String"
+  name        = "/production/database/url"
+  description = "URL for master database, for production environment"
+  value       = "thisIsTheDatabaseURL"
+  type        = "String"
+  tags = {
+    Environment = "production"
+  }
 }
