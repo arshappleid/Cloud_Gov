b81dd060e05a137079c2bbacb03767c91fd017c5
arshappleid <arsh.appleid@gmail.com>
2024-03-01 22:16:26 -0800

    Add links to open_pm_tabs.sh script

--- open_pm_tabs.sh
+++ open_pm_tabs.sh
@@ -2,3 +2,6 @@
 chrome-cli open https://github.com/arshappleid/Cloud_Gov -n
 chrome-cli open https://github.com/users/arshappleid/projects/4
 chrome-cli open https://registry.terraform.io/providers/hashicorp/aws/5.32.1/docs/resources/lb_target_group_attachment
+chrome-cli open https://www.hashicorp.com/blog/testing-hashicorp-terraform
+
+open README.md
